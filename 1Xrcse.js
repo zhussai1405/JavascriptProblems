d7c79eabd67f19b6dd52951818d2d5848858c6a1
zhussai1 <zhussai1@gmail.com>
2024-06-23 14:11:16 -0400

    ProblemSet

---
+++ 1Xrcse.js
@@ -0,0 +1,33 @@
+//Problem 
+//Write a javascript function that RETURNS the temperature converted value - give the function and the parameters an appropriate name
+//the function definition will take 2 parameters, the first parameter will be a number, 
+//the 2nd parameter will be the type of temperature which can only be C - celsius and F - Fahrenheit
+//For example if I have a function call with the following parameters -  tempConvert(32, F) will return 0, i want to know what the temp is in celsius
+//You will need to look up the formula of temperature conversion
+//Look up celsius to fahrenheith chart conversion scale
+
+
+//write function definition with 2 paramters
+
+//write steps for your logic for the conversion
+
+
+//return function value
+
+
+
+
+
+//This should return a 0 value
+//Console.log("The temperature converted from Fahrenheit to Celsius is: " + tempConvert(32, F));
+
+
+//This should return a 32 degrees value
+//Console.log("The temperature converted from Celsius to Fahrenheit is: " + tempConvert(0, C));
+
+//Console.log("The temperature converted from Celsius to Fahrenheit is: " + tempConvert(42, C));
+
+
+//console.log("What is 100 degrees celsisus in fahrenheit: " + tempConvert(100, C));
+
+//console.log("What is 212 degrees fahrenheit in celsius: " + tempConvert(212, F));
---
+++ 2Xrcse.js
@@ -0,0 +1,18 @@
+//Write a JavaScript function to get the last element of an array. 
+//Passing the parameter 'n' will return the last 'n' elements of the array.
+//For Example, function name 
+//lstArrayElement([1,2,3,4]); returns 4
+//lstArrayElement(['a','b','c','d','e']); returns e
+//lstArrayElement(['one','two','three','four','five']); returns five
+
+
+
+
+
+
+
+
+
+
+//Write a javascript function that concatenates all string elements of an array
+//
