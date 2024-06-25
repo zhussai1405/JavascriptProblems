705382b2a424a16c05ac279435b458b1ef91f7ae
Faraz Hassan Qureshi <farazhassan748@gmail.com>
2024-06-25 02:35:49 -0700

    iinit commit
    commit changes

--- 1Xrcse.js
+++ 1Xrcse.js
@@ -8,8 +8,28 @@
 
 
 //write function definition with 2 paramters
+function tempConvert(temp , type){                      //Create a function that return the two parameters 
+    if(type === 'F'){                                   //condition to check the type of temp, 1st we take fahreheight
+        // Convert Celsius to Fahrenheit
+        return (temp - 32) * 5/9                       //Method applied to convert temp celsius to fahrenheit
+
+    }else if (type === 'C') {                           //condition to check the type of temp, 2nd we take celsius 
+        // Convert Fahrenheit to Celsius
+        return (temp * 9/5) + 32;                     //Method applied to convert temp fahreheight into the celsius 
+
+
+    }else {
+        //Invalid type
+        return('Invalid type');
+    }
+
+}
 
 //write steps for your logic for the conversion
+//function define tempreature convert with two parameters value 32, and type is F/C.
+//function check the value of type celsius or fahrenhight
+//if the type is in fahrenhigh than we using the formula F= (C*9/5)+32
+//if the type is in celsius than we using the formula C = (F-32)*5/9
 
 
 //return function value
@@ -17,17 +37,17 @@
 
 
 
-
 //This should return a 0 value
-//Console.log("The temperature converted from Fahrenheit to Celsius is: " + tempConvert(32, F));
+console.log("The temperature converted from Fahrenheit to Celsius is: " + tempConvert(32, 'F'));
 
 
 //This should return a 32 degrees value
-//Console.log("The temperature converted from Celsius to Fahrenheit is: " + tempConvert(0, C));
+console.log("The temperature converted from Celsius to Fahrenheit is: " + tempConvert(0, 'C'));
 
-//Console.log("The temperature converted from Celsius to Fahrenheit is: " + tempConvert(42, C));
+//console.log("The temperature converted from Celsius to Fahrenheit is: " + tempConvert(42, C));
 
 
 //console.log("What is 100 degrees celsisus in fahrenheit: " + tempConvert(100, C));
 
 //console.log("What is 212 degrees fahrenheit in celsius: " + tempConvert(212, F));
+ 
--- 2Xrcse.js
+++ 2Xrcse.js
@@ -5,8 +5,17 @@
 //lstArrayElement(['a','b','c','d','e']); returns e
 //lstArrayElement(['one','two','three','four','five']); returns five
 
+function lstArrayElement(arr, n=1){         //create function to get the last element of an arry 
+if(n === 1){                                //condition applied 
+    return arr[arr.length-1];               //retrun arrays length through method 
+}else{
+    return arr.slice(-n);
+}
+}
 
-
+console.log(lstArrayElement([1,2,3,4]));                                //call the function for the last element of an array
+console.log(lstArrayElement(['a','b','c','d','e']));
+console.log(lstArrayElement(['one','two','three','four','five']));
 
 
 
@@ -15,4 +24,11 @@
 
 
 //Write a javascript function that concatenates all string elements of an array
-//
+function concatStringElements(arr) {                                            //create javascript function co cancat string element 
+    return arr.filter(item => typeof item === 'string').join('');               //create method to concat the arrays 
+
+}
+
+console.log(concatStringElements(['one','two','3','4']));                       //call the function 
+console.log(concatStringElements(['a','b','c','d','e']));
+console.log(concatStringElements(['one','two','three','four','five']));
