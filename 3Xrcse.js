17daed659cf6a565beca8771366ae3ab28ad670e
zhussai1 <zhussai1@gmail.com>
2024-06-25 22:00:19 -0400

    Sample problems for objects

---
+++ 3Xrcse.js
@@ -0,0 +1,79 @@
+//Practice questions for Objects
+
+
+// Create an object called 'person' with properties: name, age, and city
+// Access and log each property to the console
+
+
+
+
+
+// Add a new property 'job' to the 'person' object
+// Modify the 'age' property to be 2 years older
+// Log the updated object to the console
+
+
+
+
+// Create an object using computed property names
+// Use a variable 'key' set to "favoriteColor"
+// Set the value of this computed property to "blue"
+
+
+
+
+// Create variables 'name' and 'age' with values
+// Use property value shorthand to create an object 'user' with these properties
+// Log each property name and value to the console
+
+
+
+
+// Check if the 'person' object has a property 'gender'
+// Use both the 'in' operator and comparing with undefined
+// Log the results to the console
+
+
+
+
+
+
+
+// Use a for...in loop to iterate over all properties of the 'person' object
+// Log each property name and value to the console
+
+
+
+
+
+
+// Add a method 'greet' to the 'person' object that logs a greeting message
+// Call the 'greet' method
+
+
+
+
+
+
+
+// Create a shallow copy of the 'person' object using Object.assign()
+// Modify a property in the copy and log both objects to see the difference
+
+
+
+
+
+
+
+// Create an object 'company' with nested objects for 'department' and 'employee'
+// Access and log a nested property
+
+
+
+
+
+
+
+// Use object destructuring to extract 'name' and 'age' from the 'person' object
+// Log the extracted values
+
